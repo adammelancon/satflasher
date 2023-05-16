6a00dc4083506f3516a962581ccef9c3f564fa01
Adam Melancon <adammelancon@gmail.com>
2023-05-16 16:40:50 -0500

    Create wifi.py

---
+++ wifi.py
@@ -0,0 +1,36 @@
+import network
+import time
+
+# Wi-Fi configuration
+ssid_list = ['SSID1', 'SSID2']  # List of Wi-Fi SSIDs to try
+password_list = ['', 'PASSWORD2']  # List of passwords for each Wi-Fi SSID.  SSID1 is open network.
+
+
+# Wireless ########################################
+def connect_to_wifi(ssid, password):
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    station.connect(ssid, password)
+    print(f'Attempting connection to "{ssid}"...')
+    for _ in range(10):  # Try to connect for 10 seconds
+        if station.isconnected():
+            print(f"Connected to {ssid}.")
+            print("")
+
+            return True
+        time.sleep(1)
+    else:
+        print('Failed to connect to', ssid)
+        station.disconnect()
+        station.active(False)
+        return False
+
+
+def connect_to_wifi_networks():
+    for i in range(len(ssid_list)):
+        ssid = ssid_list[i]
+        password = password_list[i]
+        if connect_to_wifi(ssid, password):
+            return True
+    return False
+# Wireless ########################################
